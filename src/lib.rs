84561e0572d2c8deabc617a569f359a21bcfdfb1
Ivan Vasiunyk <i.vasiunyk@gmail.com>
2023-12-11 23:53:25 +0100

    Add Point and Vector structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
+pub type Tuple4X = (f64, f64, f64, f64);
+
+#[derive(Debug)]
+pub struct Point(pub Tuple4X);
+
+impl Point {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Point((x,y,z,1.0))
+    }
+}
+
+impl Default for Point {
+    fn default() -> Self {
+        Point::new(f64::default(), f64::default(), f64::default())
+    }
+}
+
+#[derive(Debug)]
+pub struct Vector(pub Tuple4X);
+
+impl Vector {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Vector((x, y, z, 0.0))
+    }
+}
+
+impl Default for Vector {
+    fn default() -> Self {
+        Vector::new(f64::default(), f64::default(), f64::default())
+    }
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
---
+++ tests/tuples.rs
@@ -0,0 +1,133 @@
+use std::num::ParseFloatError;
+use std::str::FromStr;
+use cucumber::{given, then, World, Parameter};
+use ray_tracer_challenge::{Point, Tuple4X, Vector};
+
+#[derive(Debug, Default, Parameter)]
+#[param(regex = r"tuple\((.*)\)")]
+struct Tuple4(Tuple4X);
+
+impl FromStr for Tuple4 {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let result: Result<Vec<f64>, ParseFloatError> = s.split(',').map(str::trim).map(|x_str| x_str.parse::<f64>()).collect();
+
+        if let Ok(v) = result {
+            return Ok(Tuple4((v[0], v[1], v[2], v[3])))
+        }
+
+        Err(format!("Failed to parse \"{}\" as tuple", s))
+    }
+}
+
+#[derive(Debug, Default, Parameter)]
+#[param(regex = r"tuple\((.*)\)")]
+struct Tuple3((f64, f64, f64));
+
+impl FromStr for Tuple3 {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let result: Result<Vec<f64>, ParseFloatError> = s.split(',').map(str::trim).map(|x_str| x_str.parse::<f64>()).collect();
+
+        if let Ok(v) = result {
+            return Ok(Tuple3((v[0], v[1], v[2])))
+        }
+
+        Err(format!("Failed to parse \"{}\" as tuple", s))
+    }
+}
+
+#[derive(Debug, Parameter)]
+#[param(regex = "x|y|z|w")]
+enum Axis {
+    X,
+    Y,
+    Z,
+    W,
+}
+
+impl FromStr for Axis {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "x" => Self::X,
+            "y" => Self::Y,
+            "z" => Self::Z,
+            "w" => Self::W,
+            invalid => return Err(format!("Invalid `Axis`: {invalid}")),
+        })
+    }
+}
+
+#[derive(Debug, Default, World)]
+pub struct TupleWorld {
+    tuple: Tuple4X,
+    point: Point,
+    vector: Vector,
+}
+
+#[given(expr = "a ← {tuple4}")]
+fn tuple4x(world: &mut TupleWorld, tuple: Tuple4) {
+    world.tuple = tuple.0;
+}
+
+#[given(expr = "p ← point\\({float}, {float}, {float}\\)")]
+fn point(world: &mut TupleWorld, x: f64, y: f64, z: f64) {
+    world.point = Point::new(x, y, z);
+}
+
+#[given(expr = "a ← point.tuple")]
+fn point_tuple(world: &mut TupleWorld) {
+    world.tuple = world.point.0;
+}
+
+#[given(expr = "v ← vector\\({float}, {float}, {float}\\)")]
+fn vector(world: &mut TupleWorld, x: f64, y: f64, z: f64) {
+    world.vector = Vector::new(x, y, z);
+}
+
+#[given(expr = "a ← vector.tuple")]
+fn vector_tuple(world: &mut TupleWorld) {
+    world.tuple = world.vector.0;
+}
+
+#[then(expr = "a.{axis} = {float}")]
+fn axis_value(world: &mut TupleWorld, axis: Axis, val: f64) {
+    match axis {
+        Axis::X => {assert_eq!(world.tuple.0, val)}
+        Axis::Y => {assert_eq!(world.tuple.1, val)}
+        Axis::Z => {assert_eq!(world.tuple.2, val)}
+        Axis::W => {assert_eq!(world.tuple.3, val)}
+    }
+}
+
+#[then(regex = r"^a is (not |)a point$")]
+fn is_point(world: &mut TupleWorld, state: String) {
+    match state.as_str() {
+        "not " => assert_ne!(world.tuple.3, 1.0),
+        _ => assert_eq!(world.tuple.3, 1.0)
+    }
+}
+
+#[then(regex = "^a is (not |)a vector$")]
+fn is_vector(world: &mut TupleWorld, state: String) {
+    match state.as_str() {
+        "not " => assert_ne!(world.tuple.3, 0.0),
+        _ => assert_eq!(world.tuple.3, 0.0),
+    }
+}
+
+#[then(expr = "p = {tuple4}")]
+fn point_tuple_val_is(world: &mut TupleWorld, tuple: Tuple4) {
+    assert_eq!(world.point.0, tuple.0)
+}
+
+#[then(expr = "v = {tuple4}")]
+fn vector_tuple_val_is(world: &mut TupleWorld, tuple: Tuple4) {
+    assert_eq!(world.vector.0, tuple.0)
+}
+
+fn main() {
+    futures::executor::block_on(TupleWorld::run("tests/features/tuples"));
+}
