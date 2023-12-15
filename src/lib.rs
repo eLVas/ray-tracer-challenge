1249015ab8bc5ee88d7be70e7aaff673263b5961
Ivan Vasiunyk <i.vasiunyk@gmail.com>
2023-12-15 19:10:30 +0100

    feat: add tuple3x type

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,22 @@ pub use point::Point;
 pub use vector::Vector;
 
 pub type Tuple4X = (f64, f64, f64, f64);
+
+impl ApproxEq for Tuple4X {
+    fn approx_eq(&self, other: &Self) -> bool {
+        let (x, y, z, w) = self;
+        let (x_o, y_o, z_o, w_o) = other;
+
+        x.approx_eq(x_o) && y.approx_eq(y_o) && z.approx_eq(z_o) && w.approx_eq(w_o)
+    }
+}
+pub type Tuple3X = (f64, f64, f64);
+
+impl ApproxEq for Tuple3X {
+    fn approx_eq(&self, other: &Self) -> bool {
+        let (x, y, z) = self;
+        let (x_o, y_o, z_o) = other;
+
+        x.approx_eq(x_o) && y.approx_eq(y_o) && z.approx_eq(z_o)
+    }
+}
